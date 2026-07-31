320ddffb3c7a2b61bcb4abb3b105b2fa4699031a
gyana <gyana9619@gmail.com>
2026-08-01 00:03:03 +0530

    production work

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ let app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://ai-resume-analyzer-app-five.vercel.app"],
     credentials: true,
   }),
 );
