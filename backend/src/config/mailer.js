fb0ab0ceccd2d8496ca9d5924d060b2dd2b9a6e4
gyana <gyana9619@gmail.com>
2026-08-02 15:45:14 +0530

    add

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -1,14 +1,22 @@
-import "dotenv/config";
+// import "dotenv/config";
 import nodemailer from "nodemailer";
 
 const email_transport = nodemailer.createTransport({
-  service: "gmail", // 🔹 Render ke port 587 block issue ko bypass karne ke liye service use karein
-  family: 4,        // 🔹 Force IPv4
+  service: "gmail", // 🔹 Render ke port 587 block issue ko bypass karne ke liye service use karein  
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
   },
 });
+
+email_transport.verify((error, success) => {
+  if (error) {
+    console.error("❌ Email Transport Error:", error.message);  
+  } else {
+    console.log("✅ Email Transport is ready to send messages");
+  }
+});
+
 const sendEmail = async ({ to, subject, text, html }) => {
   try {
     if (!process.env.EMAIL || !process.env.PASSWORD) {
